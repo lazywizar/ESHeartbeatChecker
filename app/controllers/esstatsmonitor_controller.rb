45a6ee2a932a59d2c57e386f706f794ec8495ff7
Varun Kumar <varun@readypulse.com>
2015-02-18 10:43:54 -0800

    Adding end to end status dashboard

--- app/controllers/esstatsmonitor_controller.rb
+++ app/controllers/esstatsmonitor_controller.rb
@@ -26,9 +26,17 @@ class EsstatsmonitorController < ApplicationController
         node_stat_map['index_time_in_millis'] = value["indices"]["indexing"]["index_time_in_millis"]
         node_stat_map['delete_time_in_millis'] = value["indices"]["indexing"]["delete_time_in_millis"]
 
+        if node_stat_map['cpu_percentage'] > 300 || node_stat_map['heap_used_percent'] > 90 || node_stat_map['free_in_bytes'] < 10000
+          node_stat_map['status'] = "red"
+        else
+          node_stat_map['status'] = "green"
+        end
+
         node_stats << node_stat_map
         @stats << node_stat_map
     end
+
+    @product_avgQueryTime = HeartbeatCheckerTask.check
 
     #Rails.logger.info "Node Stats : #{node_stats}"
     #@stats = node_stats
--- app/controllers/heartbeat_checker_task.rb
+++ app/controllers/heartbeat_checker_task.rb
@@ -6,18 +6,25 @@ class HeartbeatCheckerTask
     #@ES_HOST="192.168.1.143:9200"
     @ES_HOST="ec2-23-22-8-207.compute-1.amazonaws.com:9200"
 
+    brands = ['1', '162', '1014', '1689', '843', '2818', '1919']
+
     #Stats threshold for alarm
-    product_search_max_milis = 10000
+    product_search_max_milis = 30000
 
     client = Elasticsearch::Client.new host: @ES_HOST
 
+
     #Products
     #Test 1
-    productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: '1',
+    @brand  = brands[ Random.rand(7) - 1]
+    Rails.logger.info "Random brand ID: #{@brand}"
+    puts "Random brand ID: #{@brand}"
+
+    productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: @brand,
                                              body: {
                                                  query:{
                                                      filtered: { query: { match_all: {}},
-                                                                 filter: { term: { brand_id: '1'}}}
+                                                                 filter: { term: { brand_id: @brand}}}
                                                  }
                                              }
 
@@ -39,19 +46,14 @@ class HeartbeatCheckerTask
     product_QueryTime2 = productsv2HeartBeatCheck['took']
     product_timedout2 = productsv2HeartBeatCheck['timed_out']
 
-    #Test 3
-    productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: '1',
-                                             body: {
-                                                 query:{
-                                                     filtered: { query: { match_all: {}},
-                                                                 filter: { term: { brand_id: '1'}}}
-                                                 }
-                                             }
-    Rails.logger.info productsv2HeartBeatCheck
-    product_QueryTime3 = productsv2HeartBeatCheck['took']
-    product_timedout3 = productsv2HeartBeatCheck['timed_out']
 
-    @product_avgQueryTime = (product_QueryTime1 + product_QueryTime2 + product_QueryTime3)/3
+    @product_avgQueryTime = (product_QueryTime1 + product_QueryTime2 )/2
+    Rails.logger.info "Query time : #{product_QueryTime1}, #{product_QueryTime2}"
+    Rails.logger.info "Average Query time : #{@product_avgQueryTime}"
+
+    puts "Query time : #{product_QueryTime1}, #{product_QueryTime2}"
+    puts "Average Query time : #{@product_avgQueryTime}"
+
 
     Rails.logger.info "#{@product_avgQueryTime}, #{@product_timedout} "
     if @product_avgQueryTime > product_search_max_milis           #or @product_timedout == 'true'
@@ -59,6 +61,8 @@ class HeartbeatCheckerTask
     else
       @alarm = ''
     end
+
+    return @product_avgQueryTime
 
     #Instagram
 
