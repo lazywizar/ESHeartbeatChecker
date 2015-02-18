044c6a0e2ff48b19b4af5663d2175227c5136229
Varun Kumar <varun@readypulse.com>
2015-02-18 01:56:22 -0800

    Adding Dashboard

--- app/controllers/heartbeat_checker_task.rb
+++ app/controllers/heartbeat_checker_task.rb
@@ -7,11 +7,12 @@ class HeartbeatCheckerTask
     @ES_HOST="ec2-23-22-8-207.compute-1.amazonaws.com:9200"
 
     #Stats threshold for alarm
-    product_search_max_milis = 6000
+    product_search_max_milis = 10000
 
     client = Elasticsearch::Client.new host: @ES_HOST
 
     #Products
+    #Test 1
     productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: '1',
                                              body: {
                                                  query:{
@@ -21,13 +22,39 @@ class HeartbeatCheckerTask
                                              }
 
     Rails.logger.info productsv2HeartBeatCheck
+    product_QueryTime1 = productsv2HeartBeatCheck['took']
+    product_timedout1 = productsv2HeartBeatCheck['timed_out']
 
-    @product_avgQueryTime = productsv2HeartBeatCheck['took']
-    @product_timedout = productsv2HeartBeatCheck['timed_out']
+
+    #Test 2 : TODO: Different query
+    productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: '1',
+                                             body: {
+                                                 query:{
+                                                     filtered: { query: { match_all: {}},
+                                                                 filter: { term: { brand_id: '1'}}}
+                                                 }
+                                             }
+
+    Rails.logger.info productsv2HeartBeatCheck
+    product_QueryTime2 = productsv2HeartBeatCheck['took']
+    product_timedout2 = productsv2HeartBeatCheck['timed_out']
+
+    #Test 3
+    productsv2HeartBeatCheck = client.search index: 'products_v2', type: 'product', routing: '1',
+                                             body: {
+                                                 query:{
+                                                     filtered: { query: { match_all: {}},
+                                                                 filter: { term: { brand_id: '1'}}}
+                                                 }
+                                             }
+    Rails.logger.info productsv2HeartBeatCheck
+    product_QueryTime3 = productsv2HeartBeatCheck['took']
+    product_timedout3 = productsv2HeartBeatCheck['timed_out']
+
+    @product_avgQueryTime = (product_QueryTime1 + product_QueryTime2 + product_QueryTime3)/3
 
     Rails.logger.info "#{@product_avgQueryTime}, #{@product_timedout} "
-
-    if @product_avgQueryTime > product_search_max_milis or @product_timedout == 'true' or @product_successful
+    if @product_avgQueryTime > product_search_max_milis           #or @product_timedout == 'true'
       raiseAlarm("Search on products_v2 index timed out ")
     else
       @alarm = ''
